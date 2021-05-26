93e643faf62d1f9884086e8c9424e51304fa361b
orhungorkem <gorkem.orhun98@gmail.com>
2021-05-26 20:43:23 +0300

    post fields added

--- practiceapp/src/main/java/com/practiceapp/practiceapp/entity/PostEntity.java
+++ practiceapp/src/main/java/com/practiceapp/practiceapp/entity/PostEntity.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -17,6 +20,27 @@ import java.util.List;
 @Document(collection = "posts")
 public class PostEntity {
 
+
+    @Id
+    @Field
+    private String post_id;
+
+    @Field
+    private String text;
+
+    @Field
+    private Date date;
+
+    @Field
+    private String location;
+
+    @Field
+    private String author_id;
+
+    @Field
     private List<CommunityEntity> communities = new ArrayList<>();
+
+
+
 
 }
