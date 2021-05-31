70c09b30e550190cac177dbe9e59e5d94b15fa1a
Burak Onur Duman <bdumanjc@gmail.com>
2021-05-31 20:30:18 +0300

    JoinCommunityController.java added. Related methods in service and repository classes added.

--- practiceapp/src/main/java/com/practiceapp/practiceapp/service/CommunityService.java
+++ practiceapp/src/main/java/com/practiceapp/practiceapp/service/CommunityService.java
@@ -1,8 +1,12 @@
 package com.practiceapp.practiceapp.service;
 
+import com.practiceapp.practiceapp.entity.CommunityEntity;
+import com.practiceapp.practiceapp.entity.UserEntity;
 import com.practiceapp.practiceapp.repository.CommunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommunityService {
@@ -10,4 +14,16 @@ public class CommunityService {
     @Autowired
     private CommunityRepository communityRepository;
 
+    public CommunityEntity getById(String id){
+        return communityRepository.getById(id);
+    }
+
+    public CommunityEntity addUser(UserEntity user, CommunityEntity community) {
+        List<UserEntity> members = community.getMembers();
+        members.add(user);
+        community.setMembers(members);
+        communityRepository.save(community);
+
+        return community;
+    }
 }
--- practiceapp/src/main/java/com/practiceapp/practiceapp/service/UserService.java
+++ practiceapp/src/main/java/com/practiceapp/practiceapp/service/UserService.java
@@ -1,10 +1,13 @@
 package com.practiceapp.practiceapp.service;
 
 
+import com.practiceapp.practiceapp.entity.CommunityEntity;
 import com.practiceapp.practiceapp.entity.UserEntity;
 import com.practiceapp.practiceapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -19,5 +22,18 @@ public class UserService {
 
     public UserEntity getByUserName(String userName) {
         return userRepository.getByUsername(userName);
+    }
+
+    public UserEntity getById(String id){
+        return userRepository.getById(id);
+    }
+
+    public UserEntity joinCommunity(UserEntity user, CommunityEntity community){
+        List<CommunityEntity> joinedCommunities= user.getJoined_communities();
+        joinedCommunities.add(community);
+        user.setJoined_communities(joinedCommunities);
+        userRepository.save(user);
+
+        return user;
     }
 }
