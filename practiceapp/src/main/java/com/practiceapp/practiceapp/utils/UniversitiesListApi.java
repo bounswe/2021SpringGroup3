2a1f64ba8f16ecc71048d63a9305020cca6e44d9
bekirkeldal <bekirkeldal@gmail.com>
2021-06-05 13:48:10 +0300

    Unnecessary imports deleted.

--- practiceapp/src/main/java/com/practiceapp/practiceapp/utils/UniversitiesListApi.java
+++ practiceapp/src/main/java/com/practiceapp/practiceapp/utils/UniversitiesListApi.java
@@ -1,16 +1,13 @@
 package com.practiceapp.practiceapp.utils;
 
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UniversitiesListApi {
 
+    //Returns a list of universities of a specific country in a JSONArray format.
     public String getUniversitiesList(String country) throws UnirestException {
         return Unirest.get("http://universities.hipolabs.com/search?country="+country)
              .asJson().getBody().toString();
