16cb9c57145f007e6b040b8364d020d6cc5bdade
orhungorkem <gorkem.orhun98@gmail.com>
2021-06-06 20:16:16 +0300

    Get Posts Updated

---
+++ practiceapp/src/main/java/com/practiceapp/practiceapp/controller/views/IndexViewController.java
@@ -0,0 +1,28 @@
+package com.practiceapp.practiceapp.controller.views;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexViewController {
+
+    private String baseUrl = "/home/";
+
+    private String placesUrl = baseUrl + "places/ist";  //şimdilik form lazım  daha
+
+    private String searchCommunitiesUrl = baseUrl +"communities";
+
+    //private String adviceUrl = ""
+
+    @GetMapping(path= "/home")
+    public String getIndex(Model model){
+
+        model.addAttribute("places",placesUrl);
+
+        return "index";
+    }
+
+}
