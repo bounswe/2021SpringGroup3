c431975b378c53fb4144dfba443f22c8a58e5dc9
halilbaydar <halillbaydar@gmail.com>
2021-11-03 14:24:16 +0300

    Mobile base implemented

---
+++ mobile/CmpE451_App/src/screen/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Chat({}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>BURASI CHAT SAYFASIDIR</Text>
+    </View>
+  );
+}
+
+const styles = {
+  container: {flex: 1},
+  title: {
+    
+  },
+};
---
+++ mobile/CmpE451_App/src/screen/Notification.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Notification({}) {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.title}>BURASI Notification SAYFASIDIR</Text>
+  </View>
+);
+}
+
+const styles = {
+container: {flex: 1},
+title: {
+  
+},
+};
