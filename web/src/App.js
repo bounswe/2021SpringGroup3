71ad2d3caf5e17cf3b509b806d0f785a68d39488
Ufuk-Arslan <ufukarslan99@gmail.com>
2021-11-07 04:08:56 +0300

    Implement login page design

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import Login from './components/Login';
+import React from "react";
+import 'antd/dist/antd.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/components/Login.js
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { Input, Row, Col, Button, Typography, Layout } from 'antd';
+
+import React from "react";
+
+import { LoginOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+
+const LoginButtonStyle = {
+  backgroundColor: '#00c853',
+  borderColor: '#00c853'
+};
+
+const { Text } = Typography;
+
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+  
+    const login = () => {
+      console.log('Trying to log in with', username, password);
+    }
+  
+    const onKeyPress = (e) => {
+      if (e.charCode == 13) {
+        login();
+      }
+    }
+    return (
+        <Row gutter={[0, 16]}>
+        <Col span={24}></Col>
+        <Col span={6} offset={9}>
+          <Text strong>Username or Email</Text>
+          <Input
+            placeholder="Enter username or email"
+            onKeyPress={onKeyPress}
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+          />
+        </Col>
+        <Col span={6} offset={9}>
+          <Text strong>Password</Text>
+          <Input.Password
+            placeholder="Enter password"
+            onKeyPress={onKeyPress}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
+        </Col>
+        <Col span={24} align="middle">
+          <Button style={LoginButtonStyle} type="primary" shape="round" icon={<LoginOutlined />}
+            onClick={login}>
+            Log In
+          </Button>
+        </Col>
+        <Col span={24} align="middle">
+          <Button type="link"
+            onClick={login}>
+            Forgot your password?
+          </Button>
+          <Button type="link"
+            onClick={login}>
+            Don't have an account?
+          </Button>
+        </Col>
+      </Row>
+    )
+}
+
+export default Login;
