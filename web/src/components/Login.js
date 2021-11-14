d6c77d9eb568e306386269b94e41b57a86a99793
Ufuk-Arslan <ufukarslan99@gmail.com>
2021-11-14 15:33:33 +0300

    Add forum to login component

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,71 +1,89 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Input, Row, Col, Button, Typography, Layout } from 'antd';
+import { Form, Input, Row, Col, Button, Typography, Layout, Space } from 'antd';
 
 import React from "react";
 
-import { LoginOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { LoginOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
-
-const LoginButtonStyle = {
-  backgroundColor: '#00c853',
-  borderColor: '#00c853'
-};
 
 const { Text } = Typography;
 
+const subButtonStyle = {
+  fontSize: '12px',
+  color: '#8b949e',
+  cursor: 'pointer'
+}
+
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-  
-    const login = () => {
-      console.log('Trying to log in with', username, password);
-    }
-  
-    const onKeyPress = (e) => {
-      if (e.charCode == 13) {
-        login();
-      }
-    }
-    return (
-        <Row gutter={[0, 16]}>
-        <Col span={24}></Col>
-        <Col span={6} offset={9}>
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const sendToRegistration = () => {
+    console.log('Sending to registration');
+  }
+
+  const sendToResetPassword = () => {
+    console.log('Sending to reset password');
+  }
+
+  return (
+    <Row gutter={[0, 16]}>
+      <Col span={24}></Col>
+      <Col span={6} offset={9}>
+        <Form
+          style={{marginBottom: '20px'}}
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
           <Text strong>Username or Email</Text>
-          <Input
-            placeholder="Enter username or email"
-            onKeyPress={onKeyPress}
-            onChange={e => setUsername(e.target.value)}
-            value={username}
-          />
-        </Col>
-        <Col span={6} offset={9}>
+          <Form.Item
+            name="usernameOrEmail"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your username or email.',
+              },
+            ]}
+          >
+            <Input placeholder="Enter username or email" />
+          </Form.Item>
+
           <Text strong>Password</Text>
-          <Input.Password
-            placeholder="Enter password"
-            onKeyPress={onKeyPress}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
-        </Col>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your password.',
+              },
+            ]}
+          >
+          <Input.Password placeholder="Enter password" />
+          </Form.Item>
+
+          <Col span={24} align="middle">
+            <Button type="primary" htmlType="submit" shape="round" icon={<LoginOutlined />}>
+                Login
+            </Button>
+          </Col>
+        </Form>
         <Col span={24} align="middle">
-          <Button style={LoginButtonStyle} type="primary" shape="round" icon={<LoginOutlined />}
-            onClick={login}>
-            Log In
-          </Button>
+          <Space size={'large'}>
+            <Text style={subButtonStyle} onClick={sendToResetPassword}>Forgot your password?</Text>
+            <Text style={subButtonStyle} onClick={sendToRegistration}>Don't have an account?</Text>
+          </Space>
         </Col>
-        <Col span={24} align="middle">
-          <Button type="link"
-            onClick={login}>
-            Forgot your password?
-          </Button>
-          <Button type="link"
-            onClick={login}>
-            Don't have an account?
-          </Button>
-        </Col>
-      </Row>
-    )
+      </Col >
+    </Row >
+  )
 }
 
 export default Login;
