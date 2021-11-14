ca4f652faa3989fda100b62d8a0a1c5ea719ade9
capslock35 <kaandura@gmail.com>
2021-11-15 00:20:13 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -61,15 +61,12 @@ app.get('/', function (req, res) {
   res.send('Hello');
 });
 
-// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// convert error to ApiError, if needed
 app.use(errorConverter);
 
-// handle error
 app.use(errorHandler);
 
 module.exports = app;
