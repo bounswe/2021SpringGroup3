a15d5d83d56032b035b33bc49ff1a71f791a73d2
İrem Zeynep Alagöz <irem.zeynep.alagoz@gmail.com>
2021-11-16 00:59:32 +0300

    fix settings

--- mobile/CmpE451_App/src/screen/Settings.js
+++ mobile/CmpE451_App/src/screen/Settings.js
@@ -1,29 +1,48 @@
-import { SettingsScreen } from "react-native-settings-screen"
 import React from 'react';
+
+import {
+  SectionRow,
+  SettingsPage,
+  NavigateRow,
+  BaseRow,
+} from 'react-native-settings-view';
 import {View, Text} from 'react-native';
 
-const data: SettingsData = [
-  { type: 'CUSTOM_VIEW', key: 'hero', render: this.renderHero },
-  {
-    type: 'SECTION',
-    header: 'My Section'.toUpperCase(),
-
-    rows: [
-      {
-        title: 'Account',
-        showDisclosureIndicator: true,
-      },
-      {
-        title: 'Terms and conditions',
-        showDisclosureIndicator: true,
-      },
-      {
-        title: 'Privacy Policy',
-        showDisclosureIndicator: true,
-      },
-      {
-        title: 'Delete Account',
-        showDisclosureIndicator: true,
-      },
-      { title: 'Contact us' },
-]
+export default function Profile({}) {
+  return (
+    <SettingsPage>
+      <NavigateRow
+        text="Terms and conditions"
+        leftIcon={{
+          name: 'file-document',
+          type: 'material-community',
+        }}
+        onPress={() => console.log('terms')}
+      />
+      <NavigateRow
+        text="Change Password"
+        leftIcon={{
+          name: 'folder-lock',
+          type: 'material-community',
+        }}
+        onPress={() => console.log('policy')}
+      />
+      <BaseRow
+        text="Delete Account"
+        enabled
+        leftIcon={{
+          name: 'do-not-disturb',
+          type: 'material-community',
+        }}
+      />
+      <BaseRow
+        text="Contact us"
+        leftIcon={{
+          name: 'users',
+          type: 'font-awesome',
+        }}
+        onPress={() => console.log('contact')}
+      />
+    </SettingsPage>
+  );
+}
