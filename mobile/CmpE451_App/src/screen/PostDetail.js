56d9eed839806228caa3ac2e887eea3c5db45053
kiymetakdemir <21kymet@gmail.com>
2021-11-15 06:39:04 +0300

    post details with mock data

--- mobile/CmpE451_App/src/screen/PostDetail.js
+++ mobile/CmpE451_App/src/screen/PostDetail.js
@@ -1,15 +1,250 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Linking} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import moment from 'moment';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {COLORS} from '../theme/colors';
 
-export default function Logout({}) {
+import {AXIOS_CLIENT} from '../services/axiosCientService';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ToastAndroid,
+  ScrollView,
+} from 'react-native';
+
+export default function PostDetail({route, navigation}) {
+  const { postId, communityId } = route.params;
+
+  const [postDetail, setPostDetail] = useState({});
+
+
+  useEffect(() => {
+    getPostDetail();
+  }, []);
+
+  const getPostDetail = () => {
+  AXIOS_CLIENT.get('post/detail', {
+    params: {communityId: communityId, postId: postId},
+  })
+    .then(response => {
+      if (response.status === 200) {
+        setPostDetail(response.data);
+      }else if (response.status === 404) {
+          ToastAndroid.show(response.data.message, ToastAndroid.SHORT);
+          setPostDetail(mockpostDetail);
+        } else {
+          ToastAndroid.show(TEXT.unexpectedError, ToastAndroid.SHORT);
+          setPostDetail(mockpostDetail);
+        }
+      })
+    .catch(error => {
+      console.info(error);
+      ToastAndroid.show(TEXT.networkError, ToastAndroid.SHORT);
+      setPostDetail(mockpostDetail);
+    });
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>BURASI Post Detail SAYFASIDIR</Text>
+    <View style={styles.feedItem}>
+      <ScrollView style={{flex: 1}}>
+            <Image source={{uri: mockpostDetail.userImageUrl}} style={styles.avatar} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <View>
+            <View
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'baseline',
+              }}>
+              <Text style={{fontSize: 18, color:COLORS.textColor}}>{mockpostDetail.username}</Text>
+              <Text style={{fontSize: 12, color:COLORS.textColor}}>{mockpostDetail.communityName}</Text>
+            </View>
+            <Text style={styles.timestamp}>
+              {moment(mockpostDetail.date).fromNow()}
+            </Text>
+            <Text> </Text>
+            <Text> </Text>
+          </View>
+        </View>
+        
+        <View style={styles.container}>
+        <Icon
+            name="text"
+            size={18}
+            style={{marginRight: 16}}
+          />
+        <FlatList
+          data={mockpostDetail.textFieldNames}
+          renderItem={({item}) => (
+              <View>
+                <Text style={styles.fieldName}>{item.name}</Text>
+                <Text style={styles.content}>{item.value}</Text>
+                <Text></Text>
+              </View>
+          )}
+        />
+
+        <FlatList
+          data={mockpostDetail.numberFieldNames}
+          renderItem={({item}) => (
+              <View>
+                <Text style={styles.fieldName}>{item.name}</Text>
+                <Text style={styles.content}>{item.value}</Text>
+                <Text></Text>
+              </View>
+          )}
+        />
+        <Icon
+            name="calendar"
+            size={18}
+            style={{marginRight: 16}}
+          />
+        <FlatList
+          data={mockpostDetail.dateFieldNames}
+          renderItem={({item}) => (
+              <View>
+                <Text style={styles.fieldName}>{item.name}</Text>
+                <Text style={styles.content}>{item.value}</Text>
+                <Text></Text>
+              </View>
+          )}
+        />
+
+        <Icon
+            name="link"
+            size={18}
+            style={{marginRight: 16}}
+          />
+        <FlatList
+          data={mockpostDetail.linkFieldNames}
+          renderItem={({item}) => (
+              <View>
+                <Text style={styles.fieldName}>{item.name}</Text>
+                      <TouchableOpacity
+        onPress={() => Linking.openURL(item.value)}>
+        <Text style={{color:COLORS.buttonColor}}>{item.value}</Text>
+      </TouchableOpacity>
+                <Text></Text>
+              </View>
+          )}
+        />
+
+        <Icon
+            name="location"
+            size={18}
+            style={{marginRight: 16}}
+          />
+        <FlatList
+          data={mockpostDetail.locationFieldNames}
+          renderItem={({item}) => (
+              <View>
+                <Text style={styles.fieldName}>{item.name}</Text>
+                <Text style={styles.content}>{item.value}</Text>
+                <Text></Text>
+              </View>
+          )}
+        />
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          
+          <Icon
+            name="heart"
+            size={24}
+            color={mockpostDetail.isLiked ? COLORS.buttonColor : COLORS.unlikeButtonColor}
+            style={{marginRight: 16}}
+            onPress={() => mockpostDetail.isLiked = !mockpostDetail.isLiked}
+          />
+          <Text style={styles.content}>{mockpostDetail.likeCount}</Text>
+
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = {
-  container: {flex: 1},
-  title: {},
+const mockpostDetail = {
+  "id": "",
+  "username": "zeynep",
+  "userImageUrl": 'https://drive.google.com/uc?export=view&id=1gWuxzaJRQvaEmllvUXAlEf_ClgbD6YRD',
+  "communityName": "chess",
+  "date": 1636156949000,
+  "textFieldNames": [
+    {
+      "name": "Tournament Name",
+      "value": "İstanbul Satranç Turnuvası \n*\n*\n*\n*\n*\nWe are excited to announce that we will have our first chess tournament on this Tuesday. You can see the details below. Do not forget to fill the registraion form!"
+    }
+  ],
+  "numberFieldNames": [
+    {
+      "name": "Max Capacity",
+      "value": 50
+    }
+  ],
+  "dateFieldNames": [
+    {
+      "name": "Date",
+      "value": "16.11.2021"
+    },
+    {
+      "name": "Hour",
+      "value": "18.00"
+    }
+  ],
+  "linkFieldNames": [
+    {
+      "name": "Link to registration form",
+      "value": "https://docs.google.com/forms/d/1snqCHFsaiRU1SkLfBImvuJICXH37OcwUMYta_zzDSzI/edit"
+    }
+  ],
+  "locationFieldNames": [
+    {
+      "name": "Location",
+      "value": "Beşiktaş, İstanbul"
+    }
+  ],
+  "likeCount": 67,
+  "isLiked": true
 };
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  feedItem: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 18,
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16,
+  },
+  fieldName: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#454D65',
+    fontWeight: 'bold',
+  },
+  content:{
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#454D65',
+  },
+  timestamp: {
+    fontSize: 11,
+    color: '#C4C6CE',
+    marginTop: 4,
+  },
+});
