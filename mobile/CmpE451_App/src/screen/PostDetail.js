874851c78000fc5ca186ef8cb43b9e92cc053d96
İrem Zeynep Alagöz <irem.zeynep.alagoz@gmail.com>
2021-11-16 01:17:19 +0300

    Use page variables

--- mobile/CmpE451_App/src/screen/PostDetail.js
+++ mobile/CmpE451_App/src/screen/PostDetail.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {COLORS} from '../theme/colors';
-
+import {TEXT, PAGE_VARIABLES} from '../constants';
 import {AXIOS_CLIENT} from '../services/axiosCientService';
 import {
   FlatList,
@@ -15,7 +15,8 @@ import {
 } from 'react-native';
 
 export default function PostDetail({route, navigation}) {
-  const { postId, communityId } = route.params;
+  const postId = PAGE_VARIABLES.postId;
+  const communityId = PAGE_VARIABLES.communityId;
 
   const [postDetail, setPostDetail] = useState({});
   const [refreshing, setRefreshing] = useState(false);
@@ -24,32 +25,34 @@ export default function PostDetail({route, navigation}) {
     getPostDetail();
   }, []);
 
-
-  const getPostDetail = () => {
-  AXIOS_CLIENT.get('post/detail', {
-    params: {communityId: communityId, postId: postId},
-  })
-    .then(response => {
-      if (response.status === 200) {
-        setPostDetail(response.data);
-      }else if (response.status === 404) {
-        ToastAndroid.show(response.data.message, ToastAndroid.SHORT);
-      } else {
-        ToastAndroid.show(TEXT.unexpectedError, ToastAndroid.SHORT);
-        }
-      })
-    .catch(error => {
-      console.info(error);
-      ToastAndroid.show(TEXT.networkError, ToastAndroid.SHORT);
-      setPostDetail(mockpostDetail);
-    });
+  const _onRefresh = () => {
+    setRefreshing(true);
+    getPostDetail();
+    setRefreshing(false);
   };
 
+  const getPostDetail = () => {
+    AXIOS_CLIENT.get('post/detail', {
+      params: {communityId: communityId, postId: postId},
+    })
+      .then(response => {
+        if (response.status === 200) {
+          setPostDetail(response.data);
+        } else {
+          ToastAndroid.show(response.data.message, ToastAndroid.SHORT);
+        }
+      })
+      .catch(error => {
+        console.info(error);
+        ToastAndroid.show(TEXT.networkError, ToastAndroid.SHORT);
+        setPostDetail(mockpostDetail);
+      });
+  };
 
   return (
     <View style={styles.feedItem}>
       <ScrollView>
-            <Image source={{uri: postDetail.userImageUrl}} style={styles.avatar} />
+        <Image source={{uri: postDetail.userImageUrl}} style={styles.avatar} />
         <View
           style={{
             flexDirection: 'row',
@@ -63,8 +66,12 @@ export default function PostDetail({route, navigation}) {
                 justifyContent: 'space-between',
                 alignItems: 'baseline',
               }}>
-              <Text style={{fontSize: 18, color:COLORS.textColor}}>{mockpostDetail.username}</Text>
-              <Text style={{fontSize: 12, color:COLORS.textColor}}>{mockpostDetail.communityName}</Text>
+              <Text style={{fontSize: 18, color: COLORS.textColor}}>
+                {mockpostDetail.username}
+              </Text>
+              <Text style={{fontSize: 12, color: COLORS.textColor}}>
+                {mockpostDetail.communityName}
+              </Text>
             </View>
             <Text style={styles.timestamp}>
               {moment(mockpostDetail.date).fromNow()}
@@ -73,92 +80,79 @@ export default function PostDetail({route, navigation}) {
             <Text> </Text>
           </View>
         </View>
-        
+
         <View>
-        <Icon
-            name="text"
-            size={18}
-            style={{marginRight: 16}}
-          />
-        <FlatList
-          data={mockpostDetail.textFieldNames}
-          renderItem={({item}) => (
+          <Icon name="text" size={18} style={{marginRight: 16}} />
+          <FlatList
+            refreshing={refreshing}
+            onRefresh={_onRefresh}
+            data={mockpostDetail.textFieldNames}
+            renderItem={({item}) => (
               <View>
                 <Text style={styles.fieldName}>{item.name}</Text>
                 <Text style={styles.content}>{item.value}</Text>
                 <Text></Text>
               </View>
-          )}
-        />
+            )}
+          />
 
-        <FlatList
-          data={mockpostDetail.numberFieldNames}
-          renderItem={({item}) => (
+          <FlatList
+            data={mockpostDetail.numberFieldNames}
+            renderItem={({item}) => (
               <View>
                 <Text style={styles.fieldName}>{item.name}</Text>
                 <Text style={styles.content}>{item.value}</Text>
                 <Text></Text>
               </View>
-          )}
-        />
-        <Icon
-            name="calendar"
-            size={18}
-            style={{marginRight: 16}}
+            )}
           />
-        <FlatList
-          data={mockpostDetail.dateFieldNames}
-          renderItem={({item}) => (
+          <Icon name="calendar" size={18} style={{marginRight: 16}} />
+          <FlatList
+            data={mockpostDetail.dateFieldNames}
+            renderItem={({item}) => (
               <View>
                 <Text style={styles.fieldName}>{item.name}</Text>
                 <Text style={styles.content}>{item.value}</Text>
                 <Text></Text>
               </View>
-          )}
-        />
+            )}
+          />
 
-        <Icon
-            name="link"
-            size={18}
-            style={{marginRight: 16}}
-          />
-        <FlatList
-          data={mockpostDetail.linkFieldNames}
-          renderItem={({item}) => (
+          <Icon name="link" size={18} style={{marginRight: 16}} />
+          <FlatList
+            data={mockpostDetail.linkFieldNames}
+            renderItem={({item}) => (
               <View>
                 <Text style={styles.fieldName}>{item.name}</Text>
-                      <TouchableOpacity
-        onPress={() => Linking.openURL(item.value)}>
-        <Text style={{color:COLORS.buttonColor}}>{item.value}</Text>
-      </TouchableOpacity>
+                <TouchableOpacity onPress={() => Linking.openURL(item.value)}>
+                  <Text style={{color: COLORS.buttonColor}}>{item.value}</Text>
+                </TouchableOpacity>
                 <Text></Text>
               </View>
-          )}
-        />
-
-        <Icon
-            name="location"
-            size={18}
-            style={{marginRight: 16}}
+            )}
           />
-        <FlatList
-          data={mockpostDetail.locationFieldNames}
-          renderItem={({item}) => (
+
+          <Icon name="location" size={18} style={{marginRight: 16}} />
+          <FlatList
+            data={mockpostDetail.locationFieldNames}
+            renderItem={({item}) => (
               <View>
                 <Text style={styles.fieldName}>{item.name}</Text>
                 <Text style={styles.content}>{item.value}</Text>
-                <Text></Text>
               </View>
-          )}
-        />
+            )}
+          />
         </View>
 
         <View style={{flexDirection: 'row'}}>
-          
           <Icon
             name="heart"
             size={24}
-            color={mockpostDetail.isLiked ? COLORS.buttonColor : COLORS.unlikeButtonColor}
+            color={
+              mockpostDetail.isLiked
+                ? COLORS.buttonColor
+                : COLORS.unlikeButtonColor
+            }
             style={{marginRight: 16}}
           />
           <Text style={styles.content}>{mockpostDetail.likeCount}</Text>
@@ -168,47 +162,50 @@ export default function PostDetail({route, navigation}) {
   );
 }
 const mockpostDetail = {
-  "id": "",
-  "username": "zeynep",
-  "userImageUrl": 'https://drive.google.com/uc?export=view&id=1gWuxzaJRQvaEmllvUXAlEf_ClgbD6YRD',
-  "communityName": "chess",
-  "date": 1636156949000,
-  "textFieldNames": [
+  id: '',
+  username: 'zeynep',
+  userImageUrl:
+    'https://drive.google.com/uc?export=view&id=1gWuxzaJRQvaEmllvUXAlEf_ClgbD6YRD',
+  communityName: 'chess',
+  date: 1636156949000,
+  textFieldNames: [
     {
-      "name": "Tournament Name",
-      "value": "İstanbul Satranç Turnuvası \n*\n*\n*\n*\n*\nWe are excited to announce that we will have our first chess tournament on this Tuesday. You can see the details below. Do not forget to fill the registraion form!"
-    }
+      name: 'Tournament Name',
+      value:
+        'İstanbul Satranç Turnuvası \n*\n*\n*\n*\n*\nWe are excited to announce that we will have our first chess tournament on this Tuesday. You can see the details below. Do not forget to fill the registraion form!',
+    },
   ],
-  "numberFieldNames": [
+  numberFieldNames: [
     {
-      "name": "Max Capacity",
-      "value": 50
-    }
+      name: 'Max Capacity',
+      value: 50,
+    },
   ],
-  "dateFieldNames": [
+  dateFieldNames: [
     {
-      "name": "Date",
-      "value": "16.11.2021"
+      name: 'Date',
+      value: '16.11.2021',
     },
     {
-      "name": "Hour",
-      "value": "18.00"
-    }
+      name: 'Hour',
+      value: '18.00',
+    },
   ],
-  "linkFieldNames": [
+  linkFieldNames: [
     {
-      "name": "Link to registration form",
-      "value": "https://docs.google.com/forms/d/1snqCHFsaiRU1SkLfBImvuJICXH37OcwUMYta_zzDSzI/edit"
-    }
+      name: 'Link to registration form',
+      value:
+        'https://docs.google.com/forms/d/1snqCHFsaiRU1SkLfBImvuJICXH37OcwUMYta_zzDSzI/edit',
+    },
   ],
-  "locationFieldNames": [
+  locationFieldNames: [
     {
-      "name": "Location",
-      "value": "Beşiktaş, İstanbul"
-    }
+      name: 'Location',
+      value: 'Beşiktaş, İstanbul',
+    },
   ],
-  "likeCount": 67,
-  "isLiked": true
+  likeCount: 67,
+  isLiked: true,
 };
 
 const styles = StyleSheet.create({
@@ -231,7 +228,7 @@ const styles = StyleSheet.create({
     color: '#454D65',
     fontWeight: 'bold',
   },
-  content:{
+  content: {
     fontSize: 16,
     fontWeight: '500',
     color: '#454D65',
