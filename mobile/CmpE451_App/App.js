e74b320ba4cdef7dfbe39b580b74a2048dfc9bd5
halilbaydar <halillbaydar@gmail.com>
2021-12-13 23:50:06 +0300

    warning messages removed

--- mobile/CmpE451_App/App.js
+++ mobile/CmpE451_App/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Geocoder from 'react-native-geocoding';
 import Config from 'react-native-config';
-
+import {LogBox} from 'react-native';
+LogBox.ignoreAllLogs();
 import DrawerNavigator from './src/AppNavigator';
 
 export default function App() {
