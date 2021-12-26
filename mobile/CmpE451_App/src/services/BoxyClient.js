906e40c9c2f3fa2a0020bf84e0efd6b2d2c24dd4
İrem Zeynep Alagöz <irem.zeynep.alagoz@gmail.com>
2021-12-27 00:06:27 +0300

    Add recommended communities to search page

--- mobile/CmpE451_App/src/services/BoxyClient.js
+++ mobile/CmpE451_App/src/services/BoxyClient.js
@@ -531,6 +531,24 @@ export const searchUser = async ({query}) => {
     });
 };
 
+export const getRecommendedCommunities = async () => {
+  return fetch(BASE_URL + 'communities/recommend', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Platform': 'ANDROID',
+      Authorization: await getToken(),
+    },
+  })
+    .then(response => {
+      return returnResponse(response);
+    })
+    .catch(error => {
+      console.info(error);
+      ToastAndroid.show(TEXT.networkError, ToastAndroid.SHORT);
+    });
+};
+
 const returnResponse = async response => {
   const statusCode = response.status;
   response = await response.json();
