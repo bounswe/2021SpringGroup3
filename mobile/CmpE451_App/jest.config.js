7c5ef9f31f82867f480031207e2fcd9f81eda067
İrem Zeynep Alagöz <irem.zeynep.alagoz@gmail.com>
2022-01-03 01:06:20 +0300

    Configure Jest

---
+++ mobile/CmpE451_App/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+  setupFiles: ['./jestSetupFile.js'],
+};
