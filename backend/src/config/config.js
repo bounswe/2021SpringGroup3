0b958816fbb73026b34265c618207d613f77ffe9
schroscatt <sevdesarikaya13@gmail.com>
2022-01-03 22:17:03 +0100

    config fix

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const Joi = require('joi');
 
-dotenv.config({ path: path.join(__dirname, '../../.env.example') });
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
